9dbd431834cd4a7edbbba3399486d42fa5e5b364
Zachary Casares <rrumble250@gmail.com>
2022-10-05 13:30:15 -0700

    add home page and pages_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     validates :name, presence: true
-    validates :number, presence: true
+    
     validates :email, presence: true
-    validates :password_digest, presence: true
+    
     has_secure_password
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   root "users#index"
+   root "pages#home"
    resources :users
    
    
