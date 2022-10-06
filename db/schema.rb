c6ecacf54b50ae6f93a8e871ffab662e430810c3
Zachary Casares <rrumble250@gmail.com>
2022-10-06 15:17:42 -0700

    removed columns :birthday, :numbers from tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_204343) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_164303) do
   create_table "friends", force: :cascade do |t|
     t.string "name"
     t.integer "home_number"
     t.integer "mobile_number"
     t.string "address"
     t.string "email"
-    t.string "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "number"
     t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
