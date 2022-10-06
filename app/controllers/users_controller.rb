719c235acb26abb5f3eefb31ecacad65e0995b80
Zachary Casares <rrumble250@gmail.com>
2022-10-06 15:16:14 -0700

    removed birthday column from params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:name, :email, :password_digest, :number) 
+        params.require(:user).permit(:name, :email, :password_digest) 
         
     end
 
