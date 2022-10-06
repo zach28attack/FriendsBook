04589974d42a664f18f6f9a70d03801b8052fd85
Zachary Casares <rrumble250@gmail.com>
2022-10-05 17:47:24 -0700

    add friends form and index

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,7 @@
 class FriendsController < ApplicationController
 
     def new
+        @friend = Friend.new
             
     end
 
@@ -9,22 +10,42 @@ class FriendsController < ApplicationController
     end
 
     def index
-        
+        @friends = Friend.all 
     end
 
     def edit
-            
+        @friend = Friend.find(params[:id])
+        
     end
 
     def create
+        @friend = Friend.new(friend_params)
+        if @friend.save
+            flash[:notice] = "You have successfully created a new friend contact"
+            redirect_to friends_path
+        else
+            render 'new', status: :unproccessable_entity
+        end
             
     end
 
     def update
-            
+        @friend = Friend.find(params[:id])
+        if @friend.update(friend_params)
+            flash[:notice] = "You have successfully updated a friend's contact information"
+            redirect_to friends_path
+        else
+            render 'new', status: :unproccessable_entity
+        end
     end
 
     def destroy
             
+    end
+
+    private
+    def friend_params
+        params.require(:friend).permit(:name, :home_number, :mobile_number, :address, :email, :birthday)
+        
     end
 end
