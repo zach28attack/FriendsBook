ceb8e8186f40c3dc55583d425a5badd6f68ac74e
Zachary Casares <rrumble250@gmail.com>
2022-10-07 10:53:59 -0700

    correct syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
    root "pages#home"
    resources :users
    resources :friends
-   get 'login', to: 'session#new'
-   post 'login', to: 'session#create'
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
    get 'logout', to: 'sessions#destroy'
 end
