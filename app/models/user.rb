6c8fc9994521036d90fa0b31d0412b589dac631f
Zachary Casares <rrumble250@gmail.com>
2022-10-07 10:53:03 -0700

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     validates :name, presence: true
     
     validates :email, presence: true
+
+    validates :password, presence: true
     
     has_secure_password
     has_many :friends
