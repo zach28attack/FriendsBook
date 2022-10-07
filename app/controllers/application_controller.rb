b9b5bba43193ea1930aaf9c60b6fa074952cfd3c
Zachary Casares <rrumble250@gmail.com>
2022-10-07 10:52:19 -0700

    add helper methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if  session[:user_id]
+    end
+
+    helper_method :logged_in?
+    def logged_in?
+        !!current_user
+    end
+
+    def require_user
+        if !logged_in?
+            redirect_to root_path
+            flash[:alert] = "You must be signed in to perform that action"
+        end
+        
+    end
 end
