a75be8dcdcdc503d70e3ebb27c86c05e8512e080
Zachary Casares <rrumble250@gmail.com>
2022-10-07 09:32:41 -0700

    added sessions form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
    root "pages#home"
    resources :users
    resources :friends
-   
+   get 'login', to: 'session#new'
+   post 'login', to: 'session#create'
+   get 'logout', to: 'sessions#destroy'
 end
