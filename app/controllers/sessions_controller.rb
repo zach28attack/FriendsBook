0c53499711675166abf2885fa49d595d51468587
Zachary Casares <rrumble250@gmail.com>
2022-10-07 10:50:54 -0700

    add create,destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
 
     def new
-        
-        
     end
 
     def create
-        
+        user = User.find_by(email: params[:session][:email])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash.now[:notice] = "You have successfully signed up!"
+            redirect_to user
+        else
+            render 'new', status: :unprocessable_entity
+            flash[:alert] = "There was a problem with your log in credentials"
+        end
     end
     
     def destroy
-        
+        session[:user_id] = nil
+        flash[:alert] = "Logout successful"
+        redirect_to root_path   
     end
 end
