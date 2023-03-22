2dfffbc00ddd96e13784c614dd2c6dcc3b89050f
Zachary Casares <rrumble250@gmail.com>
2023-03-22 15:41:42 -0400

    fix validations validation null attribute

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,6 @@
 class Friend < ApplicationRecord
-validates :home_number, uniqueness: true
-validates :mobile_number, uniqueness: true
+validates :home_number, uniqueness: true, allow_blank: true
+validates :mobile_number, uniqueness: true, allow_blank: true
 
 belongs_to :user
 end
