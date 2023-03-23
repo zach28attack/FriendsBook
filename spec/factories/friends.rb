baf56d38123e0eedd37078cae8d6f922472a354b
Zachary Casares <rrumble250@gmail.com>
2023-03-23 13:57:06 -0400

    create friend fixture with FactoryBot

---
+++ spec/factories/friends.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :friend do
+    name { "Johnny Bravo" }
+    address { "1234 Totally Real Rd, North Dakota, Russia" }
+    email { "email@test.com" }
+    home_number { 1-215-255-5522 }
+    mobile_number { 1-232-222-3333 }
+  end
+end
