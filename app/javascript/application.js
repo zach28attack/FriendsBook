77746be01b547613fc2e29a0ea2e4ab2cc106e71
Zachary Casares <rrumble250@gmail.com>
2023-03-24 16:10:44 -0400

    add click event to add friend nav-btn

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -15,19 +15,19 @@ dropdownToggle.addEventListener("click", () => {
   dropdownContent.classList.toggle("show");
 });
 
+//find form template
+function findFormTemplate(event, query) {
+  return event.target.closest(".card").querySelector(query).content;
+}
+
 // select all form buttons and assign event listeners
 const triggerModals = document.querySelectorAll("#trigger-modal");
-triggerModals.forEach((triggerModal) => {
-  triggerModal.addEventListener("click", function (event) {
-    // grab the template from whuch button was clicked into doc. fragment
-    const formTemplate = event.target
-      .closest(".card")
-      .querySelector("#edit-friend-form").content;
 
+triggerModals.forEach((triggerModal) => {
+  triggerModal.addEventListener("click", (event) => {
+    const formTemplate = findFormTemplate(event, "#edit_friend-form");
     // Get the form element from the fragment
     const form = formTemplate.querySelector("#form");
-    console.log(form);
-
     // Add the form to the page
     document.body.appendChild(form);
 
@@ -40,4 +40,25 @@ triggerModals.forEach((triggerModal) => {
     // when form is submitted and the window resets ,
     // set window location at the location the user clicked the form
   });
+});
+
+const addFriendTriggerModal = document.querySelector(
+  "#add_friend_trigger-modal"
+);
+addFriendTriggerModal.addEventListener("click", () => {
+  const formTemplate = document.querySelector("#new_friend-form").content;
+  // get form element from fragment
+  const form = formTemplate.querySelector("#form");
+
+  //insert form into body
+  document.body.appendChild(form);
+
+  // TODOs \\
+
+  // Add modal class to form and add page effects
+
+  // remove modal if user clicks exit button or clicks off of modal
+
+  // when form is submitted and the window resets ,
+  // set window location at the location the user clicked the form
 });
