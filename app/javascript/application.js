c58714dcd07b55429cfb60eef8d5fdc390ebe537
Zachary Casares <rrumble250@gmail.com>
2023-03-25 17:29:21 -0400

    refactor code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,69 +16,75 @@ dropdownToggle.addEventListener("click", () => {
 });
 
 //FORM MODALS
-//find form template
 function findFormTemplate(event, query) {
   return event.target.closest(".card").querySelector(query).content;
 }
+function getFormModal() {
+  return document.querySelector(".form-modal");
+}
 
-// select all form buttons and assign event listeners
-const triggerModals = document.querySelectorAll("#trigger-modal");
-
-triggerModals.forEach((triggerModal) => {
-  triggerModal.addEventListener("click", (event) => {
-    const formTemplate = findFormTemplate(event, "#edit_friend-form");
+// add event listener for edit friend
+document.addEventListener("click", (e) => {
+  // check if the clicked target matches the edit button
+  if (e.target.matches("#trigger-modal")) {
+    // grab the form inside of the template
+    const formTemplate = findFormTemplate(e, "#edit_friend-form");
     const form = formTemplate.querySelector("#form");
-    const formModal = document.querySelector(".form-modal");
-
-    if (form && !document.body.contains(formModal)) {
+    // if form isn't rendered but exists, render form modal
+    if (form && !document.querySelector(".form-modal")) {
+      // clone form and append to body
       document.body.appendChild(form.cloneNode(true));
-      form.querySelector("header").innerHTML += "<h1>Edit Friend</h1>";
+      // insert HTML to top of form modal
+      document.querySelector("#form").querySelector("header").innerHTML =
+        "<h1>Edit Friend</h1>";
     }
-    // remove form modal
-    document.querySelector(".form-modal").addEventListener("click", (event) => {
-      const formModal = document.querySelector(".form-modal");
-      if (formModal) {
-        document.body.removeChild(formModal);
+
+    // close modal if user clicks off of form
+    getFormModal().addEventListener("click", (e) => {
+      if (e.target === document.querySelector(".form-modal").children[0]) {
+        // remove duplicated form from body
+        document.body.removeChild(getFormModal());
       }
     });
 
+    // close modal if user presses escape key
     document.addEventListener("keydown", (event) => {
       if (event.key === "Escape") {
-        const formModal = document.querySelector(".form-modal");
-        if (formModal) {
-          document.body.removeChild(formModal);
-        }
-      }
-    });
-  });
-});
-
-const addFriendTriggerModal = document.querySelector(
-  "#add_friend_trigger-modal"
-);
-addFriendTriggerModal.addEventListener("click", () => {
-  // display form modal
-  const formTemplate = document.querySelector("#new_friend-form").content;
-  const form = formTemplate.querySelector("#form");
-  const formModal = document.querySelector(".form-modal");
-  if (form && !document.body.contains(formModal)) {
-    document.body.appendChild(form.cloneNode(true));
-    document.querySelector("#form").querySelector("header").innerHTML +=
-      "<h1>Add Friend</h1>";
-
-    // remove form modal
-    document.querySelector(".form-modal").addEventListener("click", (event) => {
-      document.querySelector("#form").remove(form);
-      console.log(event.target);
-    });
-
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") {
-        const formModal = document.querySelector(".form-modal");
-        if (formModal) {
-          document.body.removeChild(formModal);
+        if (getFormModal()) {
+          // remove duplicated form from body
+          document.body.removeChild(getFormModal());
         }
       }
     });
   }
 });
+
+document
+  .querySelector("#add_friend_trigger-modal")
+  .addEventListener("click", () => {
+    // display form modal
+    const formTemplate = document.querySelector("#new_friend-form").content;
+    const form = formTemplate.querySelector("#form");
+    // if form isn't rendered but exists, render form modal
+    if (form && !document.body.contains(getFormModal())) {
+      document.body.appendChild(form.cloneNode(true));
+      document.querySelector("#form").querySelector("header").innerHTML +=
+        "<h1>Add Friend</h1>";
+
+      // remove form modal
+      getFormModal().addEventListener("click", (e) => {
+        if (e.target === getFormModal()) {
+          // remove duplicated form from body
+          document.body.removeChild(getFormModal());
+        }
+      });
+
+      document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+          if (getFormModal()) {
+            document.body.removeChild(getFormModal());
+          }
+        }
+      });
+    }
+  });
